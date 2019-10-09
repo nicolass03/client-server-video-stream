5ba43cf1f4929a70077a0292906ce71d6df700c5
Usuario de Salon <aulas@academico.icesi.edu.co>
2019-10-09 14:58:29 -0500

    Fixed client handling. Server and client finished.

--- udp_client.py
+++ udp_client.py
@@ -13,16 +13,22 @@ port = 49000
 
 def start_video():
     #cv2.startWindowThread()
-    while True:
-        data,client = s.recvfrom(2**16)
-        print(data)
-        nparr = np.frombuffer(pickle.loads(data), np.uint8)
-        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        #encoded_frame = data[HEADERSIZE:]
-        #encoded_frame = data[1]
-        #frame = base64.b64decode(encoded_frame)
-        cv2.imshow('Ad', frame)
-        cv2.waitKey(1)
+    try:
+        while True:
+            data, client = s.recvfrom(2**16)
+            print(data)
+            nparr = np.frombuffer(pickle.loads(data), np.uint8)
+            frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+            #encoded_frame = data[HEADERSIZE:]
+            #encoded_frame = data[1]
+            #frame = base64.b64decode(encoded_frame)
+            cv2.imshow('Ad', frame)
+            cv2.waitKey(1)
+    except Exception as e:
+        print(e)
+
+    cv2.destroyAllWindows()
+    s.close()
 
 def main():
     s.sendto(bytes("connect", "utf-8"),(ip,port))
--- udp_server.py
+++ udp_server.py
@@ -2,6 +2,8 @@ import socket
 import cv2
 import pickle
 import time
+import threading
+from _thread import *
 import base64
 
 HEADERSIZE = 4
@@ -11,35 +13,83 @@ ip = "localhost"
 port = 49000
 
 try:
-    s.bind((ip,port))
-    print("[+] Server started on "+ip)
+    s.bind((ip, port))
+    print("[+] Server started on " + ip)
 except socket.error as e:
-    print("[-] Couldn't start server on "+ip)
+    print("[-] Couldn't start server on " + ip)
 connected_clients = set()
+video_frames = []
 cap = cv2.VideoCapture('video.mp4')
 
-def client_thread(address):
-    for i in range(0,2048):
+
+def init_video():
+    print("[+] Server is loading the video, please wait...")
+    # Read until video is completed 
+    for i in range(0, 4096):
         if not cap.isOpened():
             print("Error loading the file.")
             break
-
         else:
             ret, frame = cap.read()
             if ret:
                 ret, buffer = cv2.imencode('.jpg', frame)
                 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-                #encoded_frame = base64.b64encode(buffer)
-                #data = f"{i:04d}"+str(encoded_frame)
+                # encoded_frame = base64.b64encode(buffer)
+                # data = f"{i:04d}"+str(encoded_frame)
                 data = cv2.imencode('.jpg', frame, encode_param)[1].tostring()
-                s.sendto(pickle.dumps(data), address)
+                video_frames.append(data)
+
+    # When everything done, release  
+    # the video capture object 
+    cap.release()
+    print("[+] Video is loaded, server ready to stream...")
+    main()
+
+
+def client_thread(address):
+    try:
+        for x in video_frames:
+            s.sendto(pickle.dumps(x), address)
+            time.sleep(0.016)
+        connected_clients.remove(address)
+    except Exception as e:
+        print(e)
+
+
+"""
+def client_thread(address):
+    try:
+        for i in range(0,2048):
+            if not cap.isOpened():
+                print("Error loading the file.")
+                break
+            else:
+                ret, frame = cap.read()
+                if ret:
+                    ret, buffer = cv2.imencode('.jpg', frame)
+                    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+                    #encoded_frame = base64.b64encode(buffer)
+                    #data = f"{i:04d}"+str(encoded_frame)
+                    data = cv2.imencode('.jpg', frame, encode_param)[1].tostring()
+                    s.sendto(pickle.dumps(data), address)
+    except Exception as e:
+        print(e)
+
+"""
+
 
 def main():
     while True:
-        data,client = s.recvfrom(2**16)
+        data, client = s.recvfrom(2 ** 16)
         if not client in connected_clients:
-            print("[+] Now streaming to {client}")
+            print("[+] Now streaming to " + str(client[0]))
             connected_clients.add(client)
-            client_thread(client)
+            t = threading.Thread(
+                target=client_thread,
+                args=(client,)
 
-main()
+            )
+            t.start()
+
+
+init_video()
